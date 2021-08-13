b2d0c0a49820219ec4e5071064005de91880874e
Seven Du <shiweidu@outlook.com>
2021-08-13 12:51:04 +0800

    Add contact us section

--- components/home/call-to-action.tsx
+++ components/home/call-to-action.tsx
@@ -20,7 +20,7 @@ export default function HomeCATSection() {
             </div>
             <div className="ml-3 inline-flex rounded-md shadow">
               <a
-                href="#"
+                href="#cantact-us"
                 className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-indigo-600 bg-white hover:bg-indigo-50"
               >
                 更多
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import HomeMissionSection from '../components/home/mission'
 import HomeHeroSection from '../components/home/hero'
 import HomeCATSection from '../components/home/call-to-action'
+import HomeContactsSection from '../components/home/contact-section'
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <HomeHeroSection />
       <HomeMissionSection />
       <HomeCATSection />
+      <HomeContactsSection />
     </>
   )
 }
