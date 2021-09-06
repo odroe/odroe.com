90f0f6516dc3b786eb4b2480e4c1ed5e46bbd26f
Seven Du <shiweidu@outlook.com>
2021-09-06 10:44:31 +0800

    *

--- components/footer.tsx
+++ components/footer.tsx
@@ -10,7 +10,13 @@ export default function Footer() {
       </div>
       <div className="text-center pb-6 text-gray-700 dark:text-gray-400">
           <p>&copy; {now} 四川耦左科技有限公司</p>
-          <p><a href="https://beian.miit.gov.cn/" target="_blank" rel="noopener noreferrer">蜀ICP备2021021384号-2</a></p>
+          <p>
+            <a href="https://beian.miit.gov.cn/" target="_blank" rel="noopener noreferrer">蜀ICP备2021021384号-2</a>
+            <a className="ml-4" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=51019002004080" target="_blank" rel="noopener noreferrer">
+              <img src="/beian.png" className="inline w-5" />
+              川公网安备51019002004080号
+            </a>
+          </p>
       </div>
     </footer>
   )
