706f33c641404c4ce9a78d9e3b1bc3ef615f2c31
Seven Du <whine.whitish.0u@icloud.com>
2022-11-11 19:02:41 +0800

    chore: remove unused com[onent

--- components/navigation.ts
+++
@@ -1,7 +0,0 @@
-export const navigation = [
-  { name: "Product", href: "/products" },
-  { name: "Developer", href: "/developer" },
-  { name: "Blog", href: "/blog" },
-  { name: "Use Cases", href: "/showcase" },
-  { name: "Company", href: "/company" },
-]
