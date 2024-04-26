274b62d9557bed16e3a8e8faf5b022c38f90bde9
Seven Du <whine.whitish.0u@icloud.com>
2024-04-27 02:00:12 +0800

    chore: *

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script>
+  import Header from '$lib/widgets/header.svelte';
+  import '../app.css';
+</script>
+
+<svelte:head>
+  <title>Odroe: Create user interfaces from Setup-widget</title>
+  <meta
+    name="description"
+    content="A declarative, efficient, and flexible Flutter UI framework for building user interfaces."
+  />
+</svelte:head>
+
+<Header />
+
+<slot />
