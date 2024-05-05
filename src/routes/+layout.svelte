8c10933d6289e2b65ad5fc1cdc6e8cb5b9ebed38
Seven Du <whine.whitish.0u@icloud.com>
2024-05-05 12:30:24 +0800

    chore: WIP

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import logo from '$lib/logo.svg?url';
+</script>
+
+<svelte:head>
+  <link rel="shortcut icon" href={logo} type="image/svg+xml" color="red" />
+</svelte:head>
+
+<slot />
